c111f5e604571b4f07b98d6dff4095f31ba1a202
santacruz-24 <it.santacruz24@google.com>
2024-10-08 11:18:38 +0530

    commit

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     # MongoDB configuration
-    MONGO_URI = os.getenv("MONGO_URI", "")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://jijinjebanesh:XJdnv0shvUxXihqs@fertioptimizer.gjsww.mongodb.net/?retryWrites=true&w=majority&appName=FertiOptimizer")
